255392b79243f296e3f0988bc1ea12d371a606d3
Mateus Carvalho <Mateusfc2004@gmail.com>
2025-04-16 14:55:55 -0300

    Finalizando a lading page com todas as áreas inseridas

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// validation form
+const firstName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const btn_submit = document.querySelector("#btnSubmit");
+
